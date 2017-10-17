8aa4c2e7b167a1949c56af46b216d39c874b8c5a
Damiaan Dufaux <damiaan@dpro.be>
2017-10-18 00:37:53 +0200

    Add private key

--- Sources/Run/main.swift
+++ Sources/Run/main.swift
@@ -2,8 +2,18 @@ import Vapor
 import App
 
 let drop = try Droplet()
-drop.get { request in
-	return try createCalendar()
+drop.get("*") { request in
+	return "The vapor hosted version of KulCal is currently only in private beta."
+}
+
+if let privateKey = drop.config["keys", "damiaan"]?.string {
+	drop.get("key", privateKey) { request in
+		return createCalendar()
+	}
+} else {
+	drop.get("key", "*") { request in
+		return "Key not found. Make sure to set up a private key in the keys config file."
+	}
 }
 
 try drop.run()
